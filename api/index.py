e3c1b73ee80ca70f74d2dd3b89ed9487107c9fae
HIMANSHU POSWAL <himanshuposwal2008@gmail.com>
2025-09-07 08:52:13 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -15,7 +15,6 @@ def get_content_from_request():
 
 @app.route("/")
 def index():
-    return "Hi there"
     return jsonify(status="success", message="Welcome to the Kerna AI API!")
 
 @app.route("/upload", methods=['POST'])
